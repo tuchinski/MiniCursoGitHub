ed54ced8242f87c36446a3072213a5ee4ab5d99c
Leonardo Tuchinski <tuchinski@alunos.utfpr.edu.br>
2016-04-07 11:15:12 -0300

    Segundo commit

---
+++ testeGitHub.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Outro exemplo de codigo\nDigite um numero:";
+    cin >> n;
+    cout << "\nO numero digitado foi: "<<n;
+}
